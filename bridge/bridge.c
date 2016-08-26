98369e44e85b3ccae123a1488e76ab743ac93139
scottinet <scottinet@kaliop.com>
2016-08-26 12:39:48 +0200

    WIP: prepared shared lib version of espresso

---
+++ bridge/bridge.c
@@ -0,0 +1,143 @@
+/*
+  Entry point of the espresso source code.
+  Replaces main.c
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "bridge.h"
+#include "espresso.h"
+
+char ** get_solution(pPLA);
+void add_to_buffer(char **, unsigned int *, unsigned int*, char);
+
+char ** minimize(char ** data, unsigned int length) {
+  pPLA PLA;
+  bool error;
+  cost_t cost;
+  pcover fold;
+  char ** result = NULL;
+
+  if (length == 0) {
+    return NULL;
+  }
+
+  /*
+   espresso reads either from a file or from stdin, so we
+   start by filling up the stdin buffer with our PLA data
+   */
+  for(int i = 0; i < length; ++i) {
+    fwrite(data[i], strlen(data[i]), 1, stdin);
+  }
+
+  if (read_pla(stdin, TRUE, TRUE, FD_type, &PLA) == EOF) {
+    return NULL;
+  }
+
+  PLA->filename = "(stdin)";
+
+  fold = sf_save(PLA->F);
+  PLA->F = espresso(PLA->F, PLA->D, PLA->R);
+  EXECUTE(error = verify(PLA->F, fold, PLA->D), VERIFY_TIME, PLA->F, cost);
+
+  if (error) {
+    PLA->F = fold;
+    (void) check_consistency(PLA);
+  } else {
+    free_cover(fold);
+    result = get_solution(PLA);
+  }
+
+  /* cleanup all used memory */
+  free_PLA(PLA);
+  FREE(cube.part_size);
+  setdown_cube();             /* free the cube/cdata structure data */
+  sf_cleanup();               /* free unused set structures */
+  sm_cleanup();               /* sparse matrix cleanup */
+
+  return result;
+}
+
+char ** get_solution(pPLA PLA) {
+  register pcube last, p;
+  char ** solutions = malloc(((PLA->F)->count +1) * sizeof(char*));
+  unsigned int current_solution = 0, buffer_size = 128;
+  char *buffer = malloc(buffer_size); // dynamic alloc to allow realloc if necessary
+
+  if (solutions == NULL || buffer == NULL) {
+    fprintf(stderr, "[espresso-logic-minimizer] FATAL: memory allocation failed\n");
+
+    if (solutions) free(solutions);
+    if (buffer) free(buffer);
+    return NULL;
+  }
+
+  // we set the last item +1 to NULL to let caller functions know when to stop
+  // reading the array
+  solutions[(PLA->F)->count] = NULL;
+
+  foreach_set(PLA->F, last, p) {
+    unsigned int cursor = 0;
+
+    /*
+      Rewrite of cvrout.c:print_cube to replace stdout printing with
+      variable storage
+     */
+     int i, var;
+
+     for(var = 0; var < cube.num_binary_vars; var++) {
+       add_to_buffer(&buffer, &buffer_size, &cursor, "?01-" [GETINPUT(p, var)]);
+     }
+
+     for(var = cube.num_binary_vars; var < cube.num_vars - 1; var++) {
+       add_to_buffer(&buffer, &buffer_size, &cursor, ' ');
+
+       for(i = cube.first_part[var]; i <= cube.last_part[var]; i++) {
+         add_to_buffer(&buffer, &buffer_size, &cursor, "01" [is_in_set(p, i) != 0]);
+       }
+     }
+
+     if (cube.output != -1) {
+       int last_part = cube.last_part[cube.output];
+      add_to_buffer(&buffer, &buffer_size, &cursor, ' ');
+
+       for(i = cube.first_part[cube.output]; i <= last_part; i++) {
+         add_to_buffer(&buffer, &buffer_size, &cursor, "01" [is_in_set(p, i) != 0]);
+       }
+     }
+
+     solutions[current_solution] = malloc(cursor + 1);
+
+     if (solutions[current_solution] == NULL) {
+       fprintf(stderr, "[espresso-logic-minimizer] FATAL: memory allocation failed\n");
+
+       for(int j = 0; j < current_solution; j++) free(solutions[j]);
+       free(buffer);
+       return NULL;
+     }
+
+     buffer[cursor] = '\0';
+     strcpy(solutions[current_solution], buffer);
+     current_solution++;
+  }
+
+  free(buffer);
+  return solutions;
+}
+
+void add_to_buffer(char ** buffer, unsigned int *buffer_size, unsigned int* cursor, char chr) {
+  if (*cursor + 1 > *buffer_size) {
+    char * new_buffer = realloc(*buffer, *buffer_size * 2);
+
+    if (new_buffer == NULL) {
+      fprintf(stderr, "[espresso-logic-minimizer] FATAL: memory allocation failed\n");
+      return;
+    }
+
+    *buffer_size *= 2;
+    *buffer = new_buffer;
+  }
+
+  (*buffer)[*cursor] = chr;
+  (*cursor)++;
+}
---
+++ bridge/bridge.h
@@ -0,0 +1,7 @@
+#ifndef __ESPRESSO_BRIDGE_H
+#define __ESPRESSO_BRIDGE_H
+
+extern void init_runtime(void);
+char ** minimize(char ** data, unsigned int length);
+
+#endif
