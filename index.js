deb1be387aa1c3aa08d73c73b4d1e04233350d17
scottinet <scottinet@kuzzle.io>
2016-09-27 13:15:30 +0200

    throw Error objects instead of strings

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function doMinimization(data) {
   var badContent;
 
   if (!Array.isArray(data)) {
-    throw 'EspressoLogicMinimizer: expected an array, got a ' + typeof data;
+    throw new Error('EspressoLogicMinimizer: expected an array, got a ' + typeof data);
   }
 
   badContent = data.filter((element) => typeof element != 'string');
 
   if (badContent.length > 0) {
-    throw 'EspressoLogicMinimizer: incorrect data content. Only strings are supported: ' + badContent;
+    throw new Error('EspressoLogicMinimizer: incorrect data content. Only strings are supported: ' + badContent);
   }
 
   return EspressoLogicMinimizer.minimize(data);
